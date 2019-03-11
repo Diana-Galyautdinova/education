e26940ddbf5012643a48d393cfe2a8feace173ec
Diana <galevut_koshca@mail.ru>
2019-03-11 20:45:21 +0300

    добавили отображение видео и гифки

--- app/Services/Faker/Provider/Video.php
+++ app/Services/Faker/Provider/Video.php
@@ -8,14 +8,22 @@ class Video extends Base
 {
     public function video($query = 'kitten')
     {
-        $url = 'https://pixabay.com/api/videos/?' . http_build_query([
+        $url = 'https://pixabay.com/api/videos/?'.http_build_query([
                 'key' => config('services.pixabay.key'),
                 'q' => $query,
                 'per_page' => 200,
                 'page' => 1,
             ]);
 
-        $videos = json_decode(file_get_contents($url))->hits;
+        $i = 0;
+        do {
+            try {
+                $videos = json_decode(file_get_contents($url))->hits;
+                break;
+            } catch (\Throwable $e) {
+                info('Hmm..', ['e' => $e]);
+            }
+        } while ($i++ < 10);
 
         return $videos[self::numberBetween(1, count($videos) - 1)]->videos->medium->url;
     }
